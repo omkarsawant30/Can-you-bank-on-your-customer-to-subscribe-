2f12de6058c2e0df789c350574f84036abec005d
Omkar Sawant <37528117+omkarsawant30@users.noreply.github.com>
2018-04-02 13:25:49 +0530

    Update Bank_RF.R

--- Bank_RF.R
+++ Bank_RF.R
@@ -1,3 +1,4 @@
+#First we need to set the directory of the file's location in your computer and then read it so that R can download the data
 setwd("E:/DATA ANALYTICS JOURNEY/R Edvancer/PROJECT 5 BANKING") 
 
 bf_train=read.csv("bank-full_train.csv",sep=",",header=T)
@@ -5,6 +6,9 @@ bf_test=read.csv("bank-full_test.csv",sep=",",header=T)
 library(dplyr)
 glimpse(bf_train)
 
+##You will need same set of vars on both train and test,its easier to manage that if you combine train and test
+##in the beginning and then separate them once you are done with data preparation
+##We'll fill test's response column with NAs.
 bf_test$y= NA
 bf_train$data = 'train'
 bf_test$data = 'test'
@@ -13,9 +17,11 @@ all= rbind(bf_train,bf_test)
 
 apply(all,2,function(x) length(unique(x)))
 
+#We remove ID and poutcome as they are not useful in predictions
 all=all %>% 
   select(-ID,-poutcome)
 
+#Next we'll create dummy variables for remaining categorical variables
 CreateDummies=function(data,var,freq_cutoff=100){
   t=table(data[,var])
   t=t[t>freq_cutoff]
@@ -47,18 +53,19 @@ cat_cols
 cat_cols=cat_cols[!(cat_cols %in% c('data','y'))]
 cat_cols
 
+# we are using frequency cutoff as 50, there is no magic number here,
+# lower cutoffs will simply result in more number of dummy variables
 for(col in cat_cols){
   all=CreateDummies(all,col,50)
 }
 
-
-for(col in names(all)){
-  
-  if(sum(is.na(all[,col]))>0 & !(col %in% c("data","y"))){
-    
+#Remove NA values from the data except the response column 
+for(col in names(all))
+{
+  if(sum(is.na(all[,col]))>0 & !(col %in% c("data","y")))
+  {
     all[is.na(all[,col]),col]=mean(all[all$data=='train',col],na.rm=T)
   }
-  
 }
 
 head(all)
@@ -74,4 +81,4 @@ fit = randomForest(as.factor(y )~ ., data = bf_train)
 
 ### Make predictions on test and submit 
 test.predictions = predict(fit, newdata = bf_test)
-write.csv(test.predictions,file = "Omkar_Sawant_P5_part2.csv", row.names = F)
+write.csv(test.predictions,file = "bankingsubs.csv", row.names = F)
